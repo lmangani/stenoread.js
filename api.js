d37aef574aad3750c7c42622f04f141f59ee2066
root <lorenzo.mangani@gmail.com>
2019-04-21 15:46:27 +0200

    fix pcap download

--- api.js
+++ api.js
@@ -2,30 +2,31 @@
 
 const fastify = require('fastify')()
 const path = require('path')
-const exec = require('child_process').exec;
+const exec = require('child_process').execSync;
 
 fastify.register(require('fastify-formbody'))
 
 fastify.register(require('fastify-static'), {
   root: path.join(__dirname, 'public'),
-  prefix: '/public/', // optional: default '/'
+  prefix: '/public/'
 })
 
 fastify.get('/', function (req, reply) {
-  console.log('got req!')
-  reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+  reply.sendFile('index.html')
 })
 
 fastify.post('/query', (req, reply) => {
    if(!req.body || !req.body.query) { reply.send('missing query!'); return; }
    console.log('Running query:',req.body.query)
-   const child = exec('node stenoread.js '+req.body.query,
-   (error, stdout, stderr) => {
-	console.log();
-	if (stdout) { reply.send(stdout) }
-	else { reply.send(error) }
-   }
-  );
+   const cmd = './stenoread.js "'+req.body.query+'"';
+   // await Query completion and return full response
+	var stdout = exec(cmd);
+	var ts = new Date().getTime();
+	reply.header('Content-disposition', 'attachment; filename= steno_'+ts+".pcap");
+        reply.type('application/octet-stream')
+	if (stdout) { reply.send( stdout ) }
+	else { console.error('failed query',req.body.query); reply.send(500) }
+
 })
 
 fastify.listen(3000, '0.0.0.0', err => {
